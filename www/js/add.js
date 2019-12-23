f937ff367755c34d34483b986a315059efdde756
k18rs044 <k18rs044@st.kyusan-u.ac.jp>
2019-12-23 14:06:07 +0900

    追加

---
+++ www/js/add.js
@@ -0,0 +1,16 @@
+// This is a JavaScript file
+var add={
+  APPLICATION_KEY:appKey,CLIENT_KEY:clientKey,
+
+  ncmb: null,
+    currentUser: null,  // ログインしたユーザーのオブジェクトを格納
+    screenSize: null,    // 画面サイズを格納
+
+    // 初期化
+    init: function(screenSize) {
+        var self = this;
+        self.ncmb = new NCMB(self.APPLICATION_KEY, self.CLIENT_KEY);    // mobile backendの初期化
+        self.screenSize = screenSize;
+    }
+
+}
